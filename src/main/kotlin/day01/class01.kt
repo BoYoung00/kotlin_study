0955b3e961ac5f93ce086228dec8cfff5487c69c
BoYoung00 <qhdud20@naver.com>
2024-01-23 16:54:15 +0900

    24/01/23 코틀린 공부 1일차

--- src/main/kotlin/day01/class01.kt
+++ src/main/kotlin/day01/class01.kt
@@ -2,14 +2,25 @@ package day01
 
 fun main() {
     var a = Person("김보영", 2000)
+    var b = Person("이두호")
 
 //    println("${a.name}, ${a.birthYear}")
 
     a.print()
+    b.print()
 }
 
 class Person (var name:String, val birthYear:Int) {
     fun print() {
         println("${name}, ${birthYear}")
+    }
+
+    init { // 생성자 초기화
+        println("init")
+    }
+
+    // 보조 생성자 (생성자 오버라이드 비슷)
+    constructor(name:String) : this(name, 1998) {
+        println("보조 생성자")
     }
 }
